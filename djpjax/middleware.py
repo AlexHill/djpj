ca929b41eeaa4012e0bf554ba75dea43dbef893e
Alex Hill <alex@hill.net.au>
2014-05-28 18:01:31 +0800

    Enable path-based PJAXing in middleware.

--- djpjax/middleware.py
+++ djpjax/middleware.py
@@ -1,7 +1,21 @@
 from djpjax.utils import strip_pjax_parameter
+from django.conf import settings
+import djpjax
 
 
 class DjangoPJAXMiddleware(object):
 
+    def __init__(self):
+        self.decorated_urls = tuple(
+            (url_prefix, getattr(djpjax, decorator)(**kwargs))
+            for url_prefix, (decorator, kwargs) in reversed(settings.DJPJ_DECORATE_URLS))
+
     def process_request(self, request):
         strip_pjax_parameter(request)
+
+    def process_template_response(self, request, response):
+        for url_prefix, decorator in self.decorated_urls:
+            if request.path.startswith(url_prefix):
+                fake_view = lambda _: response
+                response = decorator(fake_view)(request)
+        return response
