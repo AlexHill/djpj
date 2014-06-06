17cb7cbc37f9ff1077f5e618d8cd491e7266dd65
Alex Hill <alex@hill.net.au>
2014-06-06 21:24:37 +0800

    Use regexes instead of prefix matching in middleware

--- djpjax/middleware.py
+++ djpjax/middleware.py
@@ -1,3 +1,5 @@
+import re
+
 from djpjax.utils import strip_pjax_parameter
 from django.conf import settings
 import djpjax
@@ -8,15 +10,15 @@ class DjangoPJAXMiddleware(object):
     def __init__(self):
         djpjax_setting = getattr(settings, 'DJPJAX_DECORATE_URLS', [])
         self.decorated_urls = tuple(
-            (url_prefix, getattr(djpjax, decorator)(**kwargs))
-            for url_prefix, (decorator, kwargs) in reversed(djpjax_setting))
+            (re.compile(url_regex), getattr(djpjax, decorator)(**kwargs))
+            for url_regex, (decorator, kwargs) in reversed(djpjax_setting))
 
     def process_request(self, request):
         strip_pjax_parameter(request)
 
     def process_template_response(self, request, response):
-        for url_prefix, decorator in self.decorated_urls:
-            if request.path.startswith(url_prefix):
+        for url_regex, decorator in self.decorated_urls:
+            if url_regex.match(request.path):
                 fake_view = lambda _: response
                 response = decorator(fake_view)(request)
         return response
