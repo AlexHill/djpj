25598a317b272577995f3e7b76b65c1c307cfca9
Alex Hill <alex@hill.net.au>
2014-06-09 12:10:03 +0800

    Parse middleware config in separate method.

--- djpjax/middleware.py
+++ djpjax/middleware.py
@@ -7,11 +7,16 @@ import djpjax
 
 class DjangoPJAXMiddleware(object):
 
-    def __init__(self):
-        djpjax_setting = getattr(settings, 'DJPJAX_DECORATED_URLS', [])
-        self.decorated_urls = tuple(
+    def __init__(self, config=None):
+        djpjax_setting = (config or
+                          getattr(settings, 'DJPJAX_DECORATED_URLS', None))
+        self.decorated_urls = self.parse_configuration(djpjax_setting or [])
+
+    @staticmethod
+    def parse_configuration(config_tuple):
+        return tuple(
             (re.compile(url_regex), getattr(djpjax, decorator)(**kwargs))
-            for url_regex, (decorator, kwargs) in reversed(djpjax_setting))
+            for url_regex, (decorator, kwargs) in reversed(config_tuple))
 
     def process_request(self, request):
         strip_pjax_parameter(request)
